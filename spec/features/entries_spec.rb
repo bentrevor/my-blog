549c7875390f814d90913916bdb3dedbf130b2ea
Ben Trevor <benjamin.trevor@gmail.com>
2013-01-06 23:39:50 -0600

    remove commented feature spec

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -17,6 +17,12 @@ describe "Guest" do
     # expect(page).not_to have_selector("h2", text: @entry.title)
     # expect(page).not_to have_selector("p", text: @entry.content)
   end
+
+  xit "should not be able to edit an entry" do
+    visit "/entries/#{@entry.id}/edit"
+    1.should satisfy { false }
+    # expect redirection...
+  end
 end
 
 describe "Admin" do
@@ -41,93 +47,3 @@ describe "Admin" do
     expect(page).to have_selector("p", text: @entry.content)
   end
 end
-
-  
-#   scenario "as admin" do
-    
-#   end
-
-#   describe "creation" do
-#     before { visit new_entry_path }
-
-#     describe "success" do
-#       before(:each) do
-#         fill_in "Title", with: @entry.title
-#         fill_in "Content", with: @entry.content        
-#       end
-
-#       it "should add entry to database" do
-#         expect { click_button "Create Entry" }.to change(Entry, :count).by(1)
-#       end
-
-#       it "should redirect to show the new entry" do
-#         click_button "Create Entry"
-#         expect(page).to have_selector("h2", text: @entry.title)
-#         expect(page).to have_selector("p", text: @entry.content)
-#       end
-
-#       it "should show a flash[:success] message" do
-#         click_button "Create Entry"
-#         expect(page).to have_selector(".alert-success")
-#       end
-#     end
-
-#     describe "failure" do
-#       it "should not add entry to database" do
-#         fill_in "Title", with: ""
-#         fill_in "Content", with: ""
-#         expect { click_button "Create Entry" }.not_to change(Entry, :count)
-#       end
-
-#       it "should show flash[:error] message" do
-#         click_button "Create Entry"
-#         expect(page).to have_selector(".alert-error")
-#       end
-#     end
-#   end
-# end
-
-# require 'spec_helper'
-
-# describe "Entry" do
-#   before { @entry = FactoryGirl.build(:entry) }
-
-#   describe "creation" do
-#     before { visit new_entry_path }
-
-#     describe "success" do
-#       before(:each) do
-#         fill_in "Title", with: @entry.title
-#         fill_in "Content", with: @entry.content        
-#       end
-
-#       it "should add entry to database" do
-#         expect { click_button "Create Entry" }.to change(Entry, :count).by(1)
-#       end
-
-#       it "should redirect to show the new entry" do
-#         click_button "Create Entry"
-#         expect(page).to have_selector("h2", text: @entry.title)
-#         expect(page).to have_selector("p", text: @entry.content)
-#       end
-
-#       it "should show a flash[:success] message" do
-#         click_button "Create Entry"
-#         expect(page).to have_selector(".alert-success")
-#       end
-#     end
-
-#     describe "failure" do
-#       it "should not add entry to database" do
-#         fill_in "Title", with: ""
-#         fill_in "Content", with: ""
-#         expect { click_button "Create Entry" }.not_to change(Entry, :count)
-#       end
-
-#       it "should show flash[:error] message" do
-#         click_button "Create Entry"
-#         expect(page).to have_selector(".alert-error")
-#       end
-#     end
-#   end
-# end
