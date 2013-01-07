72536c53ad1d88c729ce6e53bce11c7aaf8971e3
Ben Trevor <benjamin.trevor@gmail.com>
2013-01-07 14:20:49 -0600

    update feature spec on master branch

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -1,44 +1,49 @@
 require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 
-describe "Entry" do
-  before { @entry = FactoryGirl.build(:entry) }
+describe "Guest" do
+  before(:each) do
+    @entry = FactoryGirl.create(:entry)
+  end
 
-  describe "creation" do
-    before { visit new_entry_path }
+  it "should be able to read entries" do
+    visit "/entries/#{@entry.id}"
+    expect(page).to have_selector("h2", text: @entry.title)
+    expect(page).to have_selector("p", text: @entry.content)
+  end
 
-    describe "success" do
-      before(:each) do
-        fill_in "Title", with: @entry.title
-        fill_in "Content", with: @entry.content        
-      end
+  it "should not see admin links" do
+    # expect(page).not_to have_selector("h2", text: @entry.title)
+    # expect(page).not_to have_selector("p", text: @entry.content)
+  end
 
-      it "should add entry to database" do
-        expect { click_button "Create Entry" }.to change(Entry, :count).by(1)
-      end
+  xit "should not be able to edit an entry" do
+    visit "/entries/#{@entry.id}/edit"
+    1.should satisfy { false }
+    # expect redirection...
+  end
+end
 
-      it "should redirect to show the new entry" do
-        click_button "Create Entry"
-        expect(page).to have_selector("h2", text: @entry.title)
-        expect(page).to have_selector("p", text: @entry.content)
-      end
+describe "Admin" do
+  before(:each) do
+    @entry = FactoryGirl.create(:entry)
+    user = FactoryGirl.create(:user)
+    user.add_role :admin
+    login_as(user, scope: user)
+  end
 
-      it "should show a flash[:success] message" do
-        click_button "Create Entry"
-        expect(page).to have_selector(".alert-success")
-      end
-    end
+  after(:each) do
+    Warden.test_reset!
+  end
 
-    describe "failure" do
-      it "should not add entry to database" do
-        fill_in "Title", with: ""
-        fill_in "Content", with: ""
-        expect { click_button "Create Entry" }.not_to change(Entry, :count)
-      end
+  it "should see admin links" do
+    pending
+  end
 
-      it "should show flash[:error] message" do
-        click_button "Create Entry"
-        expect(page).to have_selector(".alert-error")
-      end
-    end
+  it "should be able to read entries" do
+    visit "/entries/#{@entry.id}"
+    expect(page).to have_selector("h2", text: @entry.title)
+    expect(page).to have_selector("p", text: @entry.content)
   end
 end
