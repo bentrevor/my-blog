f94af1c262fbedf1f068bd6f00f816ca7b259686
Ben Trevor <benjamin.trevor@gmail.com>
2013-01-07 21:47:11 -0600

    add pending expectations to entries feature spec (again)

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -2,48 +2,51 @@ require 'spec_helper'
 include Warden::Test::Helpers
 Warden.test_mode!
 
-describe "Guest" do
-  before(:each) do
-    @entry = FactoryGirl.create(:entry)
+describe "Visiting site" do
+  describe "as guest", type: :feature do
+    let(:entry){ FactoryGirl.create(:new_entry) }
+
+    it "should be able to read entries" do
+      visit "/entries/#{entry.id}"
+      expect(page).to have_selector("h2", text: entry.title)
+      expect(page).to have_selector("p", text: entry.content)
+    end
+
+    it "should not be able to edit an entry" do
+      visit "/entries/#{entry.id}/edit"
+      pending
+      # expect redirection...
+    end
+
+    it "should not see admin links"
+    it "should be able to see index of all entry titles"
+    it "should not be able to write a new entry"
+    it "should not be able to visit any user page"
+
   end
 
-  it "should be able to read entries" do
-    visit "/entries/#{@entry.id}"
-    expect(page).to have_selector("h2", text: @entry.title)
-    expect(page).to have_selector("p", text: @entry.content)
-  end
+  describe "as admin", type: :feature do
+    let(:entry){ FactoryGirl.create(:new_entry) }
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      user.add_role :admin
+      login_as(user, scope: user)
+    end
 
-  it "should not see admin links" do
-    # expect(page).not_to have_selector("h2", text: @entry.title)
-    # expect(page).not_to have_selector("p", text: @entry.content)
-  end
+    after(:each) do
+      Warden.test_reset!
+    end
 
-  xit "should not be able to edit an entry" do
-    visit "/entries/#{@entry.id}/edit"
-    1.should satisfy { false }
-    # expect redirection...
-  end
-end
+    it "should be able to read entries" do
+      visit "/entries/#{entry.id}"
+      expect(page).to have_selector("h2", text: entry.title)
+      expect(page).to have_selector("p", text: entry.content)
+    end
 
-describe "Admin" do
-  before(:each) do
-    @entry = FactoryGirl.create(:entry)
-    user = FactoryGirl.create(:user)
-    user.add_role :admin
-    login_as(user, scope: user)
-  end
+    it "should see admin links"
+    it "should be able to edit an entry"
+    it "should be able to delete an entry"
+    it "should be able to write a new entry"
 
-  after(:each) do
-    Warden.test_reset!
-  end
-
-  it "should see admin links" do
-    pending
-  end
-
-  it "should be able to read entries" do
-    visit "/entries/#{@entry.id}"
-    expect(page).to have_selector("h2", text: @entry.title)
-    expect(page).to have_selector("p", text: @entry.content)
   end
 end
