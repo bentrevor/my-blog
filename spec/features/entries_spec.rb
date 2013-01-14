f8de03269fa0d2d9de4eab204cc717cfd07614ae
Ben Trevor <benjamin.trevor@gmail.com>
2013-01-14 00:35:15 -0600

    finish entries feature spec for admins

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "Visiting site", type: :feature do
-  # ensures the entries database always has one entry
   before { @first_entry = FactoryGirl.create(:first_entry) }
   let(:entry) { FactoryGirl.create(:entry) }
   let(:new_entry) { FactoryGirl.create(:new_entry) }
@@ -21,14 +20,12 @@ describe "Visiting site", type: :feature do
       new_entry = FactoryGirl.create(:new_entry)
       visit entries_path
 
-      # expect(page).to have_selector("li.no-bullet", text: entry.title)
-      # expect(page).to have_selector("li.no-bullet", text: new_entry.title)
-      find_link(entry.title).should be_visible
+      find_link(@first_entry.title).should be_visible
       find_link(new_entry.title).should be_visible
     end
 
     it "should be able to follow sidebar links" do
-      new_entry = FactoryGirl.create(:new_entry)
+      new_entry
       visit root_path
       click_link "Newest Entry"
       expect(current_path).to eq("/entries/#{new_entry.id}")
@@ -39,20 +36,18 @@ describe "Visiting site", type: :feature do
     end
 
     it "should not see admin links" do
-      new_entry
       visit entry_path(new_entry)
       expect(page).not_to have_selector("a.btn")
     end
 
     it "should not be able to visit any user page" do
-      # expect_redirect_away_from "/users/sign_in"
       expect_redirect_away_from "/users/sign_up"
       expect_redirect_away_from "/users/edit"
     end
   end
 
   describe "as admin" do
-    let(:entry){ FactoryGirl.create(:new_entry) }
+    let(:entry){ FactoryGirl.create(:entry) }
     before(:each) do
       user = FactoryGirl.create(:user)
       user.add_role :admin
@@ -69,7 +64,6 @@ describe "Visiting site", type: :feature do
     end
 
     it "should see admin links" do
-      # new_entry
       visit entry_path(new_entry)
 
       expect(page).to have_selector("p", text: "Signed in as")
@@ -79,10 +73,30 @@ describe "Visiting site", type: :feature do
     end
 
     it "should be able to edit an entry" do
-      pending
+      edited_title = "edited title"
+      edited_content = "edited content"
+
+      visit entry_path(@first_entry)
+      click_link "Edit this entry"
+      fill_in "Title", with: edited_title
+      fill_in "Content", with: edited_content
+      click_button "Update Entry"
+
+      expect(page).to have_selector("h2", text: edited_title)
+      expect(page).to have_selector("p", text: edited_content)
     end
-    # it "should be able to delete an entry"
-    # it "should be able to write a new entry"
+
+    it "should be able to delete an entry" do
+      visit entry_path(entry)
+      expect { click_link "Delete this entry" }.to change { Entry.count }.by(-1)
+    end
+
+    it "should be able to write a new entry" do
+      click_link "Write new entry"
+      fill_in "Title", with: "hey"
+      fill_in "Content", with: "hey"
+      expect { click_button "Create Entry" }.to change { Entry.count }.by(1)
+    end
 
   end
 end
