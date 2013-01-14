5c93a1e6c9b060af93b0ddffada0c58186216b9c
Ben Trevor <benjamin.trevor@gmail.com>
2013-01-14 01:48:50 -0600

    update master branch

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
-include Warden::Test::Helpers
-Warden.test_mode!
 
-describe "Visiting site" do
-  # ensures the entries database always has one entry
+describe "Visiting site", type: :feature do
   before { @first_entry = FactoryGirl.create(:first_entry) }
   let(:entry) { FactoryGirl.create(:entry) }
   let(:new_entry) { FactoryGirl.create(:new_entry) }
 
-  describe "as guest", type: :feature do
+  describe "as guest" do
     it "should be able to read entries" do
       visit "/entries/#{entry.id}"
       expect(page).to have_selector("h2", text: entry.title)
@@ -20,50 +17,44 @@ describe "Visiting site" do
     end
 
     it "should be able to see index of all entry titles" do
-      entry
-      new_entry
+      new_entry = FactoryGirl.create(:new_entry)
       visit entries_path
 
-      expect(page).to have_selector("li.no-bullet", text: entry.title)
-      expect(page).to have_selector("li.no-bullet", text: new_entry.title)
-      find_link(entry.title).should be_visible
+      find_link(@first_entry.title).should be_visible
       find_link(new_entry.title).should be_visible
     end
 
     it "should be able to follow sidebar links" do
       new_entry
       visit root_path
-      click_link "Newest Entry"
+      click_on "Newest Entry"
       expect(current_path).to eq("/entries/#{new_entry.id}")
-      click_link "About"
+      click_on "About"
       expect(current_path).to eq("/about")
-      click_link "Archives"
+      click_on "Archives"
       expect(current_path).to eq("/entries")
     end
 
     it "should not see admin links" do
-      new_entry
       visit entry_path(new_entry)
       expect(page).not_to have_selector("a.btn")
     end
 
     it "should not be able to visit any user page" do
-      expect_redirect_away_from "/users/sign_in"
       expect_redirect_away_from "/users/sign_up"
       expect_redirect_away_from "/users/edit"
     end
   end
 
-  describe "as admin", type: :feature do
-    let(:entry){ FactoryGirl.create(:new_entry) }
+  describe "as admin" do
+    let(:entry){ FactoryGirl.create(:entry) }
     before(:each) do
       user = FactoryGirl.create(:user)
       user.add_role :admin
-      login_as(user, scope: user)
-    end
-
-    after(:each) do
-      Warden.test_reset!
+      visit new_user_session_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Sign in"
     end
 
     it "should be able to read entries" do
@@ -72,18 +63,39 @@ describe "Visiting site" do
       expect(page).to have_selector("p", text: entry.content)
     end
 
-    xit "should see admin links" do
-      new_entry
+    it "should see admin links" do
       visit entry_path(new_entry)
+
+      expect(page).to have_selector("p", text: "Signed in as")
       expect(page).to have_selector("a.btn", text: "Write new entry")
       expect(page).to have_selector("a.btn", text: "Edit this entry")
       expect(page).to have_selector("a.btn", text: "Delete this entry")
     end
 
+    it "should be able to edit an entry" do
+      edited_title = "edited title"
+      edited_content = "edited content"
 
-    # it "should be able to edit an entry"
-    # it "should be able to delete an entry"
-    # it "should be able to write a new entry"
+      visit entry_path(@first_entry)
+      click_on "Edit this entry"
+      fill_in "Title", with: edited_title
+      fill_in "Content", with: edited_content
+      click_on "Update Entry"
 
+      expect(page).to have_selector("h2", text: edited_title)
+      expect(page).to have_selector("p", text: edited_content)
+    end
+
+    it "should be able to delete an entry" do
+      visit entry_path(entry)
+      expect { click_on "Delete this entry" }.to change { Entry.count }.by(-1)
+    end
+
+    it "should be able to write a new entry" do
+      click_on "Write new entry"
+      fill_in "Title", with: "bubbles"
+      fill_in "Content", with: "bubbles"
+      expect { click_on "Create Entry" }.to change { Entry.count }.by(1)
+    end
   end
 end
