ebaeeddcbd783685cd19139e7b77a52e4d2c01e8
Ben Trevor <btrevor@8thlight.com>
2014-09-20 00:59:43 -0500

    Remove failing specs, fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Blog::Application.routes.draw do
   root to: 'entries#index'
 
-  resources :entries
+  get '/archives',    to: 'entries#index'
+  get '/entries/:id', to: 'entries#show'
 
   get "/about",       to: 'status#about'
   get "/tic-tac-toe", to: 'status#tic_tac_toe'
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,148 +1,16 @@
 require 'spec_helper'
 
 describe EntriesController do
-  it "can GET index" do
+  it 'can GET index' do
     get :index
 
     expect(response.status).to be 200
   end
 
-  describe "show action" do
-    before { get :show, id: @entry }
+  it 'can show an entry' do
+    entry = Entry.create :title => 'title', :content => 'content'
+    get :show, :id => entry.id
 
-    it "should return http success and render :show template" do
-
-      get :show, :id => 1
-
-      response.should be_success
-      response.should render_template :show
-    end
-
-    it "should assign @entry based on params" do
-      assigns(:entry).should eq(@entry)
-
-      new_entry = FactoryGirl.create(:new_entry)
-      get :show, id: new_entry
-      assigns(:entry).should eq(new_entry)
-    end
-  end
-
-  describe "new action" do
-    before { get :new }
-
-    it "should return http success and render :new template" do
-      response.should be_success
-      response.should render_template :new
-    end
-
-    it "should assign @entry to a new record" do
-      assigns(:entry).should be_new_record
-    end
-  end
-
-  describe "create action" do
-    describe "with valid attributes" do
-      before { @attrs = FactoryGirl.attributes_for(:new_entry) }
-
-      it "should add entry to database" do
-        expect {
-          post :create, entry: @attrs
-        }.to change { Entry.count }.by(1)
-      end
-
-      it "should redirect to that entry's show page" do
-        post :create, entry: @attrs
-        response.should redirect_to Entry.last
-      end
-    end
-
-    describe "with invalid attributes" do
-      it "should not add entry to database" do
-        expect { post :create }.not_to change { Entry.count }
-        expect { post :create, Entry.new(title: "test for blank content")
-                                          }.not_to change { Entry.count }
-
-        expect { post :create, Entry.new(content: "test for blank title")
-                                          }.not_to change { Entry.count }
-      end
-
-      it "should re-render the new action" do
-        post :create, entry: Entry.new
-        response.should render_template :new
-      end
-    end
-  end
-
-  describe "edit action" do
-    it "should return http success" do
-      get :edit, id: @entry
-      response.should be_success
-    end
-  end
-
-  describe "update action" do
-    before { @attrs = FactoryGirl.attributes_for(:new_entry) }
-
-    describe "with valid attributes" do
-      before { put :update, id: @entry, entry: @attrs }
-
-      it "should locate the correct entry" do
-        assigns(:entry).should eq(@entry)
-      end
-
-      it "should change attributes of entry" do
-        @entry.reload
-        @entry.title.should eq("new title")
-        @entry.content.should eq("new content")
-      end
-
-      it "should redirect to updated entry" do
-        response.should redirect_to @entry
-      end
-    end
-
-    describe "with invalid attributes" do
-      before { put :update, id: @entry, entry: { title: "", content: "" } }
-
-      it "should not modify the entry" do
-        @entry.reload
-        @entry.title.should eq("factory entry title")
-        @entry.content.should eq("factory entry content")
-      end
-
-      it "should re-render the edit method" do
-        response.response_code.should eq(200)
-        response.should render_template :edit
-      end
-    end
-  end
-
-  describe "destroy action" do
-    it "should remove the entry from the database" do
-      expect {
-        delete :destroy, id: @entry
-      }.to change { Entry.count }.by(-1)
-    end
-
-    it "should redirect to index" do
-      delete :destroy, id: @entry
-      response.should redirect_to entries_url
-    end
-  end
-
-  describe "about action" do
-    it "should return http success and render :about template" do
-      get :about
-      response.should be_success
-      response.should render_template :about
-    end
-  end
-
-  describe "tictactoe action" do
-    it "should return http success and render :tictactoe template" do
-      get :tic_tac_toe
-      response.should be_success
-      response.should render_template :tic_tac_toe
-    end
+    expect(response.status).to be 200
   end
 end
