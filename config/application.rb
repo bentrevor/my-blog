c8dc220894cac86fd59bb7c01cb87d2440c35c7a
Ben Trevor <btrevor@8thlight.com>
2014-09-19 23:55:04 -0500

    Upgrade gems

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "sprockets/railtie"
 
 if defined?(Bundler)
@@ -13,7 +12,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-    
+
 module Blog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -32,12 +31,6 @@ module Blog
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blog::Application.routes.draw do
   root to: 'entries#index'
 
   resources :entries
-  match "/about", to: 'entries#about'
 
-  match "/tic-tac-toe", to: 'entries#tic_tac_toe'
+  get "/about", to: 'entries#about'
+  get "/tic-tac-toe", to: 'entries#tic_tac_toe'
 end
